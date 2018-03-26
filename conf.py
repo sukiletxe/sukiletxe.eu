8d5bdfe71e77bf62876f596c332da31af7ef63d8
Sukil Etxenike <sukiletxe@yahoo.es>
2018-03-26 15:03:31 +0200

    Changed SITE_URL

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Sukil Etxenike"  # (translatable)
 BLOG_TITLE = "What was your name, again?"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://sukiletxe.gitlab.io/"
+SITE_URL = "https://www.sukiletxe.eu/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com"
