bafa9d30fd1347adb8357b2bc97bce373675e722
Sukil Etxenike <sukiletxe@yahoo.es>
2018-08-17 11:56:08 +0200

    Do not always schedule

--- conf.py
+++ conf.py
@@ -1093,7 +1093,7 @@ DEPLOY_DRAFTS = False
 # Specify an iCal Recurrence Rule: http://www.kanzaki.com/docs/ical/rrule.html
 SCHEDULE_RULE = 'RRULE:FREQ=WEEKLY;BYDAY=SU;BYHOUR=12;BYMINUTE=0;BYSECOND=0'
 # If True, use the scheduling rule to all posts (not pages!) by default
-SCHEDULE_ALL = True
+SCHEDULE_ALL = False
 
 # Do you want a add a Mathjax config file?
 # MATHJAX_CONFIG = ""
