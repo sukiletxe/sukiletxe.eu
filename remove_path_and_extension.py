2d64d82ad82a9635ad08d35129c790970b18da56
Sukil Etxenike <sukiletxe@yahoo.es>
2019-11-07 20:31:31 +0100

    Put comments in hierarchy again

--- remove_path_and_extension.py
+++ remove_path_and_extension.py
@@ -1,4 +1,4 @@
 import os
 def remove_path_and_ext(path):
-    """ Removes path and extension from comment identifier, leaving only the post slug"""
-    return os.path.splitext(os.path.split(path)[1])[0]
+    """ Removes  extension and 'cache/' from comment identifier, leaving only '/path/to/post' + the post slug"""
+    return os.path.splitext(path.replace('cache/', ''))[0]
